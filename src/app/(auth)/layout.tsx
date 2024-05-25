fa61c6bd650d1ff2fd04707acc74de4f46bca98c
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2024-05-25 14:37:40 +0900

    feat: add authentication

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-red-500">{children}</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      {children}
+    </div>
+  );
 };
 
 export default AuthLayout;
--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main className="flex flex-col">Hello</main>;
+  return (
+    <main className="flex flex-col">
+      <UserButton afterSignOutUrl="/" />
+    </main>
+  );
 }
