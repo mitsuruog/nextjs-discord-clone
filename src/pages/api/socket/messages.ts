aad5715a5146ed9c5dc75e3e7a32ff4fd659ba15
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2024-06-02 20:03:12 +0900

    feat: add message API

---
+++ src/pages/api/socket/messages.ts
@@ -0,0 +1,97 @@
+import { NextApiRequest } from "next";
+
+import { NextApiResponseServerIO } from "@/types";
+import { currentProfileForPages } from "@/lib/current-profile-for-pages";
+import { db } from "@/lib/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIO
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  try {
+    const profile = await currentProfileForPages(req);
+    const { content, fileUrl } = req.body;
+    const { serverId, channelId } = req.query;
+
+    if (!profile) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (!serverId) {
+      return res.status(400).json({ error: "Server ID missing" });
+    }
+
+    if (!channelId) {
+      return res.status(400).json({ error: "Channel ID missing" });
+    }
+
+    if (!content) {
+      return res.status(400).json({ error: "Content missing" });
+    }
+
+    const server = await db.server.findFirst({
+      where: {
+        id: serverId as string,
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+
+    if (!server) {
+      return res.status(404).json({ error: "Server not found" });
+    }
+
+    const channel = await db.channel.findFirst({
+      where: {
+        id: channelId as string,
+        serverId: server.id,
+      },
+    });
+
+    if (!channel) {
+      return res.status(404).json({ error: "Channel not found" });
+    }
+
+    const member = server.members.find(
+      (member) => member.profileId === profile.id
+    );
+
+    if (!member) {
+      return res.status(443).json({ error: "Member not found" });
+    }
+
+    const message = await db.message.create({
+      data: {
+        content,
+        fileUrl,
+        memberId: member.id,
+        channelId: channel.id,
+      },
+      include: {
+        member: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+
+    const channelKey = `chat:${channelId}:message`;
+    res.socket.server.io.emit(channelKey, message);
+
+    return res.status(200).json(message);
+  } catch (error) {
+    console.log("[SOCKET_MESSAGES_POST]", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
